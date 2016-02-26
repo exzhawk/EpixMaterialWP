d5591d95cf96ae38ac0768f8099fc9f84f46505d
Epix <git@exz.me>
2016-02-26 15:59:57 +0800

    basic footer

--- footer.php
+++ footer.php
@@ -13,11 +13,14 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'epixmaterialwp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'epixmaterialwp' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'epixmaterialwp' ), 'epixmaterialwp', '<a href="http://exz.me/" rel="designer">Epix</a>' ); ?>
+	<footer id="colophon" class="site-footer mdl-mini-footer" role="contentinfo">
+		<div class="site-info mdl-mini-footer__left-section">
+			<ul class="mdl-mini-footer__link-list">
+				<li>
+					<a href="<?php echo esc_url(__('https://wordpress.org/', 'epixmaterialwp')); ?>"><?php printf(esc_html__('Proudly powered by %s', 'epixmaterialwp'), 'WordPress'); ?></a>
+				</li>
+				<li><?php printf(esc_html__('Theme: %1$s by %2$s.', 'epixmaterialwp'), 'epixmaterialwp', '<a href="http://exz.me/" rel="designer">Epix</a>'); ?></li>
+			</ul>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
