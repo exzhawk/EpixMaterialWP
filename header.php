a4907a8925876bbe79b2f92acb63c3fb9ed5ce14
Epix <git@exz.me>
2016-02-26 15:31:28 +0800

    basic header

--- header.php
+++ header.php
@@ -21,34 +21,30 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <a class="skip-link screen-reader-text"
        href="#content"><?php esc_html_e('Skip to content', 'epixmaterialwp'); ?></a>
 
-    <header id="masthead" class="site-header" role="banner">
-        <div class="site-branding">
+    <header id="masthead" class="site-header mdl-layout__header" role="banner">
+        <div class="mdl-layout__header-row">
             <?php
             if (is_front_page() && is_home()) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="mdl-layout-title"
+                                          rel="home"><?php bloginfo('name'); ?></a>
             <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                         rel="home"><?php bloginfo('name'); ?></a></p>
-                <?php
-            endif;
-
-            $description = get_bloginfo('description', 'display');
-            if ($description || is_customize_preview()) : ?>
-                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="mdl-layout-title"
+                                         rel="home"><?php bloginfo('name'); ?></a>
                 <?php
             endif; ?>
-        </div><!-- .site-branding -->
 
-        <nav id="site-navigation" class="main-navigation" role="navigation">
-            <button class="menu-toggle" aria-controls="primary-menu"
-                    aria-expanded="false"><?php esc_html_e('Primary Menu', 'epixmaterialwp'); ?></button>
-            <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-        </nav><!-- #site-navigation -->
+            <div class="mdl-layout-spacer"></div>
+
+            <nav id="site-navigation" class="main-navigation mdl-navigation" role="navigation">
+                <button class="menu-toggle" aria-controls="primary-menu"
+                        aria-expanded="false"><?php esc_html_e('Primary Menu', 'epixmaterialwp'); ?></button>
+                <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+            </nav><!-- #site-navigation -->
+        </div>
     </header><!-- #masthead -->
 
     <div id="content" class="site-content">
