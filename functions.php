a619fb9125597842573a3378f57b0694aa1ffd2e
Epix <git@exz.me>
2016-02-28 23:29:17 +0800

    clean up

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ function epixmaterialwp_scripts() {
 
 //	wp_enqueue_script( 'epixmaterialwp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'epixmaterialwp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+//	wp_enqueue_script( 'epixmaterialwp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
