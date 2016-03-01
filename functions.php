bffccf18eff7a9369662c420abf2c766dc4877a6
Epix <git@exz.me>
2016-03-01 23:24:08 +0800

    basic feature image

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ function epixmaterialwp_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
+	add_image_size('w400h400-thumb', 400, 400, true);
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,6 +10,10 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('mdl-card mdl-shadow--4dp'); ?>>
+<!--	<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"></a>-->
+	<div class="mdl-card__media">
+		<?php the_post_thumbnail('w400h400-thumb');?>
+	</div>
 	<div class="mdl-card__title">
 		<?php
 		if ( is_single() ) {
@@ -19,7 +23,7 @@
 		}
 		?>
 	</div>
-<!--	<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"></a>-->
+
 	<header class="entry-header">
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
