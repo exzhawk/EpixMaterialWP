3f00f3d976342f330e060df216216bd49aac9944
Epix <git@exz.me>
2016-03-01 23:56:26 +0800

    basic card components

--- template-parts/content.php
+++ template-parts/content.php
@@ -23,32 +23,16 @@
 		}
 		?>
 	</div>
-
-	<header class="entry-header">
-		<?php if ( 'post' === get_post_type() ) : ?>
+	<div class="mdl-card__supporting-text">
+		<?php echo strip_tags(get_the_content( '' )); ?>
+	</div>
+	<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php epixmaterialwp_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'epixmaterialwp' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'epixmaterialwp' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
+	endif; ?>
+	<div class="mdl-card__supporting-text">
 		<?php epixmaterialwp_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</div>
 </article><!-- #post-## -->
