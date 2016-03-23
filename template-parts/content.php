3bca6c5f2a71952412e8a339db71ca91ba19ac0a
Epix <git@exz.me>
2016-03-23 22:41:26 +0800

    card with left and right

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,36 +9,40 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('mdl-card mdl-shadow--4dp'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('mdl-card mdl-shadow--2dp epixmdl_card'); ?>>
 <!--	<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"></a>-->
-	<div class="mdl-card__media">
-		<?php the_post_thumbnail('w400h400-thumb');?>
+	<div class="epixmdl_card__left">
+		<div class="mdl-card__media">
+			<?php the_post_thumbnail('w400h400-thumb');?>
+		</div>
 	</div>
-	<div class="mdl-card__title">
-		<?php
-		if ( is_single() ) {
-			the_title( '<h2 class="entry-title mdl-card__title-text">', '</h1>' );
-		} else {
-			the_title( '<h2 class="entry-title mdl-card__title-text">', '</h2>' );
-		}
-		?>
+	<div class="epixmdl_card__right">
+        <div class="mdl-card__title">
+            <?php
+            if ( is_single() ) {
+                the_title( '<h2 class="entry-title mdl-card__title-text">', '</h1>' );
+            } else {
+                the_title( '<h2 class="entry-title mdl-card__title-text">', '</h2>' );
+            }
+            ?>
+        </div>
+        <div class="mdl-card__supporting-text">
+            <?php echo strip_tags(get_the_content( '' )); ?>
+        </div>
+        <?php if ( 'post' === get_post_type() ) : ?>
+            <div class="mdl-card__supporting-text">
+                <?php echo epixmaterialwp_get_publish_time(); ?>
+            </div>
+            <div class="mdl-card__supporting-text">
+                <?php echo epixmaterialwp_get_update_time(); ?>
+            </div>
+            <div class="mdl-card__supporting-text">
+                <?php echo epixmaterialwp_get_tags(); ?>
+            </div>
+            <div class="mdl-card__supporting-text">
+                <?php echo epixmaterialwp_get_cats(); ?>
+            </div>
+            <?php
+        endif; ?>
 	</div>
-	<div class="mdl-card__supporting-text">
-		<?php echo strip_tags(get_the_content( '' )); ?>
-	</div>
-	<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="mdl-card__supporting-text">
-			<?php echo epixmaterialwp_get_publish_time(); ?>
-		</div>
-		<div class="mdl-card__supporting-text">
-			<?php echo epixmaterialwp_get_update_time(); ?>
-		</div>
-		<div class="mdl-card__supporting-text">
-			<?php echo epixmaterialwp_get_tags(); ?>
-		</div>
-		<div class="mdl-card__supporting-text">
-			<?php echo epixmaterialwp_get_cats(); ?>
-		</div>
-		<?php
-	endif; ?>
 </article><!-- #post-## -->
